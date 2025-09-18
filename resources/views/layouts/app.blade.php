2ecb2c864c92d9d50cc8836cb78924685344a779
Sri Fauzie Kurniawan <fauziekurniawan0@gmail.com>
2025-09-18 22:01:29 +0700

    update : layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,7 @@
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
     
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.tailwindcss.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     
     @stack('scripts')
 </body>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -78,101 +78,57 @@
 
 <!-- Sidebar -->
 <aside id="logo-sidebar"
-  class="fixed top-0 left-0 z-40 w-60 h-screen pt-20 transition-transform -translate-x-full 
-         bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-  aria-label="Sidebar">
-  <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-    <ul class="space-y-2 font-medium">
-      <!-- Dashboard -->
-      <li>
-        <a href="{{ route('dashboard') }}"
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
-                 hover:bg-gray-100 dark:hover:bg-gray-700 group">
-          <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 
-                      group-hover:text-gray-900 dark:group-hover:text-white"
-            fill="currentColor" viewBox="0 0 22 21">
-            <path d="M16.975 11H10V4.025a1 1 
-                     0 0 0-1.066-.998 8.5 8.5 
-                     0 1 0 9.039 9.039.999.999 
-                     0 0 0-1-1.066h.002Z"/>
-            <path d="M12.5 0c-.157 0-.311.01-.565.027A1 
-                     1 0 0 0 11 1.02V10h8.975a1 
-                     1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 
-                     8.51 0 0 0 12.5 0Z"/>
-          </svg>
-          <span class="ms-3">Dashboard</span>
-        </a>
-      </li>
+    class="fixed top-0 left-0 z-40 w-60 h-screen pt-20 transition-transform -translate-x-full 
+           bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-medium">
+            <li>
+                <a href="{{ route('dashboard') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
+                           hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
+                    </svg>
+                    <span class="ms-3">Dashboard</span>
+                </a>
+            </li>
 
-      {{-- === Admin Menu === --}}
-      @role('admin')
-      <li>
-        <a href="{{ route('users.index') }}"
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
-                 hover:bg-gray-100 dark:hover:bg-gray-700 group">
-          <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 
-                      group-hover:text-gray-900 dark:group-hover:text-white" 
-               fill="currentColor" viewBox="0 0 20 20">
-            <path d="M14 2a3.963 3.963 0 0 0-1.4.267 
-                     6.439 6.439 0 0 1-1.331 
-                     6.638A4 4 0 1 0 14 2Z"/>
-          </svg>
-          <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
-        </a>
-      </li>
-      @endrole
+            @if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('manager') || auth()->user()->can('view attendance'))
+            <li>
+                <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-manajemen" data-collapse-toggle="dropdown-manajemen">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                      <path fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clip-rule="evenodd"/>
+                    </svg>
+                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Manajemen User</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                </button>
+                
+                <ul id="dropdown-manajemen" class="hidden py-2 space-y-2">
+                    @role('admin')
+                    <li>
+                        <a href="{{ route('users.index') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Users</a>
+                    </li>
+                    @endrole
+                    
+                    @role(['admin', 'manager'])
+                    <li>
+                        <a href="{{ route('employees.index') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Employees</a>
+                    </li>
+                    @endrole
+                    
+                    @can('view attendance')
+                    <li>
+                        <a href="{{ route('attendance.index') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Attendance</a>
+                    </li>
+                    @endcan
+                </ul>
+            </li>
+            @endif
 
-      {{-- === Employees Menu (Admin + Manager) === --}}
-      @role('admin')
-      <li>
-        <a href="{{ route('employees.index') }}"
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
-                 hover:bg-gray-100 dark:hover:bg-gray-700 group">
-          <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 
-                      group-hover:text-gray-900 dark:group-hover:text-white"
-               fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10 
-                     10 0 0 0 10 0Z"/>
-          </svg>
-          <span class="flex-1 ms-3 whitespace-nowrap">Employees</span>
-        </a>
-      </li>
-      @elserole('manager')
-      <li>
-        <a href="{{ route('employees.index') }}"
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
-                 hover:bg-gray-100 dark:hover:bg-gray-700 group">
-          <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 
-                      group-hover:text-gray-900 dark:group-hover:text-white"
-               fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 0a10 10 0 1 0 10 10A10 
-                     10 0 0 0 10 0Z"/>
-          </svg>
-          <span class="flex-1 ms-3 whitespace-nowrap">Employees</span>
-        </a>
-      </li>
-      @endrole
-
-      {{-- === Attendance Menu (Admin, Manager, Staff) === --}}
-      @can('view attendance')
-      <li>
-        <a href="{{ route('attendance.index') }}"
-          class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
-                 hover:bg-gray-100 dark:hover:bg-gray-700 group">
-          <svg class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 
-                      group-hover:text-gray-900 dark:group-hover:text-white"
-               fill="currentColor" viewBox="0 0 20 20">
-            <path d="M6 2a1 1 0 0 0-1 
-                     1v14a1 1 0 0 0 1.447.894l12-7a1 
-                     1 0 0 0 0-1.788l-12-7A1 1 
-                     0 0 0 6 2Z"/>
-          </svg>
-          <span class="flex-1 ms-3 whitespace-nowrap">Attendance</span>
-        </a>
-      </li>
-      @endcan
-
-    </ul>
-  </div>
+        </ul>
+    </div>
 </aside>
 
